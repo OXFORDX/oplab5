d57569a9234640abf6f29cd3603efb50e7ad7704
Volodya <volodka.orlov@gmail.com>
2019-06-11 01:56:36 +0300

    Created main

---
+++ main.py
@@ -0,0 +1,9 @@
+from file_parse import file_parse
+from calc import polish
+
+fi = file_parse('code.txt')
+print(polish(fi.parameters['condition']).result)
+if polish(fi.parameters['condition']).result:
+    print('Result1: ', polish(fi.parameters['then']).result)
+else:
+    print('Result2: ', polish(fi.parameters['else']).result)
